9691d4f59582ed1a9b3c592f0ff926a4c85851b2
kevenallan <kevenallan98@gmail.com>
2024-10-25 11:50:45 -0300

    first commit

---
+++ src/test/java/br/com/gerenciadordegastos/GerenciadorDeGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadordegastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
